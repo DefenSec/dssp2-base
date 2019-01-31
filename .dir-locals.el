d66312bd25f6eb6374def1c2f007949e1ac5095b
Dominick Grift <dac.override@gmail.com>
2019-01-31 09:44:10 +0100

    remove editorconfig and add dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (fill-column . 79)
+  (indent-tabs-mode))
+ (makefile-mode
+  (indent-tabs-mode . t))
+ (sh-mode
+  (sh-indentation . 4)
+  (sh-basic-offset . 4)))
